e2772b50a355078603bf03a6fafb9a0048c8519d
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-01-31 22:25:17 -0200

    Definindo Habilidades de Permissão Dinâmicamente

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,13 @@
 <div class="container">
     
     @forelse ($posts as $post)
+        <h1>{{ $post->title }}</h1>
+        <p>{{ $post->description }}</p>
+        <b>Author: {{ $post->user->name }}</b>
         @can('update-post', $post)
-            <h1>{{ $post->title }}</h1>
-            <p>{{ $post->description }}</p>
-            <b>Author: {{ $post->user->name }}</b>
             <a href="{{ url("/post/$post->id/update") }}">Editar</a>
-            <hr />
         @endcan
+        <hr />
     @empty
         <p class="text-center">Nenhum Post Cadastrado!</p>
     @endforelse
